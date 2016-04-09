4836265b5c33a8607a09121834714301a6155c70
trikitrok <trikitrok@gmail.com>
2016-04-09 16:27:10 +0200

    Passing rq04 test

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+    resources :todo_lists do
+      resources :todo_items
+    end
+
+    root to: "todo_lists#index"
 end
